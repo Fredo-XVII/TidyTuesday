37b85319362ccdb72f55365c336521caaecc7dd6
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-03-07 23:43:56 -0600

    Update EDA_n_Graphs.R

--- data/2020/2020-02-25/EDA_n_Graphs.R
+++ data/2020/2020-02-25/EDA_n_Graphs.R
@@ -1,6 +1,20 @@
 # EDA and Visualization
 
 library(tidyverse)
+library(sf)
 # Simple EDA
 
 head(measles) %>% View()
+tail(measles) %>% View()
+summary(measles)
+
+measles_base <- measles %>% 
+  dplyr::filter(!is.na(enroll),
+                !is.na(lng),
+                !is.na(lat)
+                )
+summary(measles_base)
+
+measles_base %>% 
+  ggplot(aes(x = xmed, y = xper, col = as.factor(xrel))) +
+  geom_point()
